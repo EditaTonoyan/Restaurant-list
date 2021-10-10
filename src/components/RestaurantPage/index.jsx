bbf8eadd954238f3b9c9c5028e4311b9e31f5b7c
Edita <edita.tonoyan96@gmail.com>
2021-10-10 20:12:02 +0400

    update

--- src/components/RestaurantPage/index.jsx
+++ src/components/RestaurantPage/index.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import style from "./resPage.module.css";
-import { setDoc, doc } from "firebase/firestore";
+import { setDoc, doc, addDoc, collection } from "firebase/firestore";
 import db from "firebase";
 
 const RestaurantPage = ({ match }) => {
@@ -18,17 +18,26 @@ const RestaurantPage = ({ match }) => {
     );
   }
 
-  const handleNew = async () => {
-    const docRef = doc(db, "restaurant", "oolkh");
+  const addFeedbeack = async () => {
+    const collectionRef = collection(db, "restaurant");
     const payload = {
       imag: "https://yvn.reservin.am/uploads/place/10336/modified/thumb_440x330_a56eee6e8a93d9fa3620cee019f4c88e.jpg",
       lat: 40.1893,
       lng: 44.5124,
-      stars: "9",
-      title: "Forty-Four Sky Bar",
-      vote: "2",
+      stars: "999",
+      title: "test",
+      vote: "777",
     };
-    await setDoc(docRef, payload);
+    await addDoc(collectionRef, payload);
+  };
+
+  const toRate = async (id) => {
+    const docRef = doc(db, "restaurant", id);
+    const payload = {
+      stars: "777999",
+      vote: "11111",
+    };
+    setDoc(docRef, payload);
   };
 
   return (
@@ -41,7 +50,7 @@ const RestaurantPage = ({ match }) => {
               <img className={style.mainImg} src={singleRes.image} alt="" />
               <h2>To Rate The Restourant</h2>
               {[...Array(starsCount)].map((n, i) => (
-                <div className={style.star} key={i} onClick={handleNew}></div>
+                <div className={style.star} key={i} onClick={toRate(singleRes.id)}></div>
               ))}
             </div>
           );
